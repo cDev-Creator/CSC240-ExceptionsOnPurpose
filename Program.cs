b36661ab58bc5a7cb9646899f2791e1712022f71
mothmatrix <76753452+cDev-Creator@users.noreply.github.com>
2022-10-17 14:15:19 -0400

    exercise 1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace CSC240_04_01_ExceptionsOnPurpose_NGM
+{
+    class ExceptionsOnPurpose
+    {
+        static void Main(string[] args)
+        {
+            int answer;
+            int result;
+            int zero = 0;
+
+            Write("Enter an integer >> ");
+            answer = Convert.ToInt32(ReadLine());
+
+            result = answer / zero;
+            WriteLine("The answer is " + answer);
+        }
+    }
+}
